2c93725e32c3c9f8b5bb647d2b3200153484bd92
Azarea <mairudoiron3@gmail.com>
2024-05-12 08:04:12 +0900

    テクスチャの拡大縮小のフィルターをリニアに変更

--- FDK/src/04.Graphics/CTexture.cs
+++ FDK/src/04.Graphics/CTexture.cs
@@ -455,8 +455,8 @@ namespace FDK
             //-----
 
             //拡大縮小の時の補完を指定------
-            Game.Gl.TexParameterI(GLEnum.Texture2D, GLEnum.TextureMinFilter, (int)TextureMinFilter.Nearest); //この場合は補完しない
-            Game.Gl.TexParameterI(GLEnum.Texture2D, GLEnum.TextureMagFilter, (int)TextureMinFilter.Nearest);
+            Game.Gl.TexParameterI(GLEnum.Texture2D, GLEnum.TextureMinFilter, (int)TextureMinFilter.Linear);
+            Game.Gl.TexParameterI(GLEnum.Texture2D, GLEnum.TextureMagFilter, (int)TextureMagFilter.LinearSharpenSgis);
             //------
 
             Game.Gl.BindTexture(TextureTarget.Texture2D, 0); //バインドを解除することを忘れないように
